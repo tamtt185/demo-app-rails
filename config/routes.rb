9462f93d12b94ce1bcfa99289309514e5fb3b62f
zanichi18 <thanhtam12t3@gmail.com>
2016-11-18 11:38:22 +0700

    create home page, model Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get 'users/new'
+
+  get 'users/create'
+
+  get '/help', to: 'static_pages#help'
+  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#create'
+  root 'static_pages#home'
 end
