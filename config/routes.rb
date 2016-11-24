511b88893f4ed0c73a33c73a6df2d853b237e82e
zanichi18 <thanhtam12t3@gmail.com>
2016-11-24 14:48:32 +0700

    show all entries for user (no-loggin)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/showall', to: 'entries#show'
   resources :users
   resources :entries
   resources :sessions
